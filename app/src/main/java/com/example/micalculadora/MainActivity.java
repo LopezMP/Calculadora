3188ac4c0003217682a7a52607c2300d565b4342
LopezMP <LopezMP@users.noreply.github.com>
2019-03-04 23:03:06 +0100

    code

--- app/src/main/java/com/example/micalculadora/MainActivity.java
+++ app/src/main/java/com/example/micalculadora/MainActivity.java
@@ -2,12 +2,37 @@ package com.example.micalculadora;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    Button b1;
+    Button b2;
+    Button b3;
+    Button b4;
+    Button b5;
+    Button b6;
+    Button b7;
+    Button b8;
+    Button b9;
+    Button b0;
+    Button bc;
+    Button bd;
+    Button bm;
+    Button br;
+    Button bs;
+    Button bres;
+    Button bsin;
+    Button bcos;
+    Button btan;
+    TextView text;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 }
