dd891f5d5cb22545d0309ddc340eb98db2094bbf
LopezMP <LopezMP@users.noreply.github.com>
2019-03-05 11:13:18 +0100

    declarar btn

--- app/src/main/java/com/example/micalculadora/MainActivity.java
+++ app/src/main/java/com/example/micalculadora/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.micalculadora;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -25,14 +26,128 @@ public class MainActivity extends AppCompatActivity {
     Button bsin;
     Button bcos;
     Button btan;
-    TextView text;
+    TextView pantalla;
 
+    public void init(){
+
+        b0 = findViewById(R.id.b0);
+        b1 = findViewById(R.id.b1);
+        b2 = findViewById(R.id.b2);
+        b3 = findViewById(R.id.b3);
+        b4 = findViewById(R.id.b4);
+        b5 = findViewById(R.id.b5);
+        b6 = findViewById(R.id.b6);
+        b7 = findViewById(R.id.b7);
+        b8 = findViewById(R.id.b8);
+        b9 = findViewById(R.id.b9);
+        bd = findViewById(R.id.bd);
+        bm = findViewById(R.id.bm);
+        br = findViewById(R.id.br);
+        bs = findViewById(R.id.bs);
+        bsin = findViewById(R.id.bsin);
+        bcos = findViewById(R.id.bcos);
+        btan = findViewById(R.id.btan);
+        bres = findViewById(R.id.bres);
+        bc = findViewById(R.id.bc);
+        pantalla = findViewById(R.id.text);
+
+
+    }
+
+    public void teclear(){
+        b0.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                pantalla.setText(pantalla.getText().toString()+"0");
+            }
+        });
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                pantalla.setText(pantalla.getText().toString()+"1");
+            }
+        });
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                pantalla.setText(pantalla.getText().toString()+"2");
+            }
+        });
+        b3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                pantalla.setText(pantalla.getText().toString()+"3");
+            }
+        });
+        b4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                pantalla.setText(pantalla.getText().toString()+"4");
+            }
+        });
+        b5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                pantalla.setText(pantalla.getText().toString()+"5");
+            }
+        });
+        b6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                pantalla.setText(pantalla.getText().toString()+"6");
+            }
+        });
+        b7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                pantalla.setText(pantalla.getText().toString()+"7");
+            }
+        });
+        b8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                pantalla.setText(pantalla.getText().toString()+"8");
+            }
+        });
+        b9.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                pantalla.setText(pantalla.getText().toString()+"9");
+            }
+        });
+        bd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                pantalla.setText(pantalla.getText().toString()+"/");
+            }
+        });
+        bm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                pantalla.setText(pantalla.getText().toString()+"*");
+            }
+        });
+        br.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                pantalla.setText(pantalla.getText().toString()+"-");
+            }
+        });
+        bs.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                pantalla.setText(pantalla.getText().toString()+"+");
+            }
+        });
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        init();
+        teclear();
 
     }
 }
